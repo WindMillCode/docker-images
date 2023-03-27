bfc294b9b887a82bfed1bc293662349eeaa9a422
Michael Odumosu <21322737+MichaelOdumosu57@users.noreply.github.com>
2023-03-27 19:42:08 -0400

    [UPDATE] additional work

--- docker-img-0/01-custom-docker-setup.sh
+++ docker-img-0/01-custom-docker-setup.sh
@@ -3,7 +3,7 @@ export JAVA_HOME=/root/.jabba/jdk/openjdk@1.17.0
 export PYENV_ROOT=/root/.pyenv
 export RBENV_ROOT=/usr/local/rbenv
 export NVM_DIR=/root/.nvm
-export PATH=/root/.pyenv/bin:/root/.jabba/jdk/openjdk@1.17.0/bin:/usr/bin/heroku:/root/.amplify/bin:/node_modules/.bin:/node_modules:/root/.nvm/versions/node/v14.17.3/bin:/usr/local/share/pyenv/shims:/usr/local/share/pyenv/bin:/root/.nvm:/usr/local/rbenv/bin:/usr/local/rbenv/shims:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/share/pyenv/shims:/usr/local/share/pyenv/bin:/usr/local/rbenv/shims:/usr/local/rbenv/bin:/.nvm:/usr/local/rbenv/bin:/usr/local/rbenv/shims:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+export PATH=/root/.ebcli-virtual-env/executables:/root/.pyenv/bin:/root/.jabba/jdk/openjdk@1.17.0/bin:/usr/bin/heroku:/root/.amplify/bin:/node_modules/.bin:/node_modules:/root/.nvm/versions/node/v14.17.3/bin:/usr/local/share/pyenv/shims:/usr/local/share/pyenv/bin:/root/.nvm:/usr/local/rbenv/bin:/usr/local/rbenv/shims:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/share/pyenv/shims:/usr/local/share/pyenv/bin:/usr/local/rbenv/shims:/usr/local/rbenv/bin:/.nvm:/usr/local/rbenv/bin:/usr/local/rbenv/shims:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 
 eval "$(pyenv init --path)"
--- docker-img-0/README.md
+++ docker-img-0/README.md
@@ -1,1 +1,10 @@
-has nvm,pyenv,rbenv,jabba,firefox driver,chrome driver,pyenv,azure cli and aws cli,postgresql
+
+# Changelong
+v 0.0.1
+has nvm,pyenv,rbenv,jabba,firefox driver,chrome driver,pyenv,azure cli and aws cli,
+
+v 0.0.2
+has postgressql
+
+v 0.0.4
+aws eb cli is availlble on command line
